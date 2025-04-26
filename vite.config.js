edb50e7d2dbc4d98acbf51a01c10b192de5ee719
ypsefixit <velvetjam@gmail.com>
2025-04-26 08:21:56 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
+// vite.config.js
+export default {
   base: './',
-  plugins: [react()],
-  preview: {
-    port: 4173,
-    host: '0.0.0.0',
-    allowedHosts: ['.onrender.com']
-  }
-})
+};
